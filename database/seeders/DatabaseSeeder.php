2510c71082ae5895e87104232037895246871166
NiNJiawa <152706876+NiNJiawa@users.noreply.github.com>
2025-09-16 10:47:00 +0700

    add: seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +16,19 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            'name' => 'General Admin',
+            'email' => 'ga@example.com',
+            'password' => Hash::make('password123'), // ganti sesuai kebutuhan
+            'role' => 'admin',
+        ]);
+
+        // Procurement
+        User::create([
+            'name' => 'Procurement Officer',
+            'email' => 'procurement@example.com',
+            'password' => Hash::make('password123'), // ganti sesuai kebutuhan
+            'role' => 'procurement',
         ]);
     }
 }
